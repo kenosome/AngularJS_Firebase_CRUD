98b74fcbf9d4546f971f938a161c6475289ede42
Kenny Osorio <kennyj.osorio@gmail.com>
2016-03-06 18:50:32 -0500

    Adicionar productos implementado

--- js/app.js
+++ js/app.js
@@ -3,5 +3,33 @@ var myApp = angular.module('myApp', ['firebase']);
 myApp.controller('ProductsCtrl', ['$scope','$firebaseArray', function($scope,$firebaseArray) {
         var myProducts = new Firebase('https://products-2924.firebaseio.com/products');
         $scope.products = $firebaseArray(myProducts);
+        
+        $scope.showForm= function(){
+            $scope.addFormShow=true;
+            $scope.editFormShow=false;
+            clearForm();
+        };
+        
+        $scope.hideForm=function () {
+            $scope.addFormShow=false;
+        };
+        
+        function clearForm(){
+            $scope.productName='';
+            $scope.productCode='';
+            $scope.description='';
+            $scope.price='';
+        };
+        
+        $scope.addFormSubmit=function(){
+            
+            $scope.products.$add({
+                productName:$scope.productName,
+                productCode:$scope.productCode,
+                description:$scope.description,
+                price:$scope.price                
+            });
+            clearForm();
+        };
 
 }]);
