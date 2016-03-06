3aaac590cb1cea95bd06ed9977178dfde1bd2a40
Kenny Osorio <kennyj.osorio@gmail.com>
2016-03-06 18:25:56 -0500

    Traer los productos de la base de datos Firebase

--- js/app.js
+++ js/app.js
@@ -1,31 +1,7 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['firebase']);
 
-myApp.controller('ProductsCtrl', ['$scope', function($scope) {
+myApp.controller('ProductsCtrl', ['$scope','$firebaseArray', function($scope,$firebaseArray) {
+        var myProducts = new Firebase('https://products-2924.firebaseio.com/products');
+        $scope.products = $firebaseArray(myProducts);
 
-        $scope.products = [
-            {
-                productName: "Leaf Rake",
-                productCode: "GDN0011",
-                description: "Leaf rake with 48-inch wooden handle.",
-                price: "19.95"
-            },
-            {
-                "productName": "Garden Cart",
-                "productCode": "GDN0023",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": "32.99"
-            },
-            {
-                "productName": "Hammer",
-                "productCode": "TBX0048",
-                "description": "Curved claw steel hammer",
-                "price": "8.99"
-            },
-            {
-                "productName": "Saw",
-                "productCode": "TBX0022",
-                "description": "15-inch steel blade hand saw",
-                "price": "11.55"
-            }
-        ];
 }]);
